974ea9e8d323017957d56667a061e7765a066ffa
Tarık <70340756+tarikyildiz511@users.noreply.github.com>
2020-12-14 23:38:45 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,29 +6,4 @@ for fizzbuzz in range(51):
     elif fizzbuzz %5==0:
         print("buzz")
         continue
-    print(fizzbuzz)
-for fzbz in range(51):
-    if fzbz %3 ==0 and fzbz%5 ==0:
-        print("fizzbuzz")
-    elif fzbz %3 ==0:
-        print("fizz")
-    else:
-        print("buzz")
-for fzbz in range(51):
-    if fzbz %3 ==0 and fzbz%5==0:
-        print("fizzbuzz")
-    elif fzbz %3 ==0:
-        print("fizz")
-    elif fzbz %5 == 0:
-        print("buzz")
-        continue
-    print(fzbz)
-for fizzbuzz in range(51):
-    if fizzbuzz %3==0 and fizzbuzz %5==0:
-        print("Fizzbuzz")
-    elif fizzbuzz % 3==0:
-        print("Fizz")
-    elif fizzbuzz % 5==0:
-        print("buzz")
-        continue
-    print(fizzbuzz)
+   
